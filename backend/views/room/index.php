d9d475ca3e80e4cba35a97d1ea235d4b88b471d7
tarlektarnoy <niran_tarlek@hotmail.com>
2019-09-01 22:34:01 +0700

    update

--- backend/views/room/index.php
+++ backend/views/room/index.php
@@ -85,7 +85,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     'summary' => "แสดง {begin} - {end} ของทั้งหมด {totalCount} รายการ",
                     'showOnEmpty'=>false,
                     'tableOptions' => ['class' => 'table table-hover'],
-                    'emptyText' => '<div style="color: red;text-align: center;"><br> <b>ไม่พบรายการไดๆ</b></div>',
+                    'emptyText' => '<div class="container"><div style="color: red;text-align: center;"><b>ไม่พบรายการไดๆ</b></div></div>',
                     'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
